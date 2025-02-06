2bc1929546e2f6d2b5df3db6ce339f666bee969e
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-02-06 09:14:38 +0100

    Resolver ejercicio30

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -2,11 +2,19 @@ package com.campusdual.classroom;
 
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
-
+  public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException{
+    if(divisor == 0){
+      throw new DivisionByZeroException("Excepción personalizada");
+    }else{
+      return dividend / divisor;
     }
+  }
 
-    public static void main(String[] args) {
-
+  public static void main(String[] args) {
+    try {
+      System.out.println(divisionWithCustomException(1, 0));
+    } catch (Exception e) {
+      System.out.println(e);;
     }
+  }
 }
